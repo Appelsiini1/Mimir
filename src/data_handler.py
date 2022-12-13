82dd0ce83e0a8b3586ba118ca2f4f9459d0d20ac
Rami Saarivuori <37345715+Appelsiini1@users.noreply.github.com>
2022-12-13 15:59:03 +0200

    Add path handler as a placeholder

--- src/data_handler.py
+++ src/data_handler.py
@@ -2,3 +2,16 @@
 Mímir Data Handlers
 Functions for loading and saving exercise data
 """
+
+def data_path_handler(directory_path: str):
+    """
+    Takes a general path to the cache directory and returns a dictionary of paths 
+    to the files in that directory, separated into 'general', 'metadata' and 'assignment'.
+    Individual files can be accessed via assignment ID keys.
+    For example:
+    files['assignment']['L01T1']
+
+    Params:
+    directory_path: A path to the root DATA directory in cache
+    """
+    pass
