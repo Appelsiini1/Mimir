5354a6a36fb9336cbdad9704fb78fc46e368bdcc
Rami Saarivuori <37345715+Appelsiini1@users.noreply.github.com>
2022-12-15 15:22:53 +0200

    Add wrapper for subprocess and PDF generate cmd

--- src/ext_service.py
+++ src/ext_service.py
@@ -1,5 +1,83 @@
 """
 Mímir External Service Handler
-Functions to handle connections to external services like 
+Functions to handle connections to external services like
 compilers or runtime environments
 """
+
+import logging
+import subprocess
+from os import path
+
+from constants import ENV
+from custom_errors import CannotMoveFileError
+
+def run_command(command:str, inputs:str|None, process_timeout=10):
+    """
+    Runs a command using subprocess and captures output.
+    Returns an instance of CompletedProcess if runs is a success.
+    On error returns an instance of CalledProcessError, TimeoutExpired,
+    or FileNotFoundError.
+
+    Params:
+    command: the command to run as string
+    inputs: the inputs that are given to the command or None
+    process_timeout: Defaults to 10 (seconds).
+    """
+
+    try:
+
+        output = subprocess.run(
+            command,
+            capture_output=True,
+            text=True,
+            timeout=process_timeout,
+            check=True,
+            input=inputs
+        )
+        # capture_output saves stdout ja stderr during the run
+        # Text changes bytes to str
+        # check raises CalledProcessError if exit code is not 0
+        # timeout stops the process if it takes longer than given value
+
+    except subprocess.CalledProcessError as process_error:
+        logging.exception("Process exited with non-zero code.")
+        return process_error
+    except subprocess.TimeoutExpired as timeout_exception:
+        logging.exception("Process timed out.")
+        logging.error(timeout_exception.output)
+        logging.error("##########")
+        logging.error(timeout_exception.stderr)
+        return timeout_exception
+    except FileNotFoundError as fnfe_exception:
+        logging.exception("Cannot find command")
+        return fnfe_exception
+    else:
+        logging.error("Following errors were encountered during command run: ")
+        return output
+
+
+def generate_pdf(filepath_in:str, filepath_out:str):
+    """
+    Runs pdflatex command to generate the PDF from generated TeX file.
+
+    Params:
+    filepath_in: the path to the TeX file
+    filepath_out: the path to move the finished PDF file to
+    """
+
+    #TODO change filepath_in to PROGRAM_DATA and add a 'cd' command before pdflatex and move
+    #TODO add error handling if pdflatex command returns an exception
+
+    command = f"pdflatex {filepath_in}"
+
+    output= run_command(command, None)
+
+    if output is type(subprocess.CompletedProcess):
+        if ENV["OS"] == "nt":
+            command = f"move /Y \"{filepath_in}\" \"{filepath_out}\""
+        else:
+            command = f"mv \"{filepath_in}\" \"{filepath_out}\""
+
+        output = run_command(command, None)
+        if output is not type(subprocess.CompletedProcess):
+            raise CannotMoveFileError(output)
