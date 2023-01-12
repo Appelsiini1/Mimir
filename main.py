b67af7df07477dd6ed5bb16833d42b57db6a55af
Rami Saarivuori <37345715+Appelsiini1@users.noreply.github.com>
2023-01-12 16:24:06 +0200

    Switch UI lib from Kivy to DearPyGUI

--- main.py
+++ main.py
@@ -3,23 +3,10 @@ Mímir Main
 Functions to start Mímir
 """
 
-from os import environ
-environ['KIVY_HOME'] = "./config"   # set Kivy config path
+from src.tex_generator import tex_gen, write_tex_file
 
-from kivy import require            #pylint: disable=wrong-import-position
-require('2.1.0')                    # sets the minimum version of UI library and raises an
-                                    # exception if the installed version is less than required
-
-from kivy.app import App            #pylint: disable=wrong-import-position
-from kivy.uix.label import Label    #pylint: disable=wrong-import-position
-
-
-class Mimir(App):
-    """Base class and starting script for Mímir program"""
-    def build(self):
-        return Label(text='Hello world!')
-
+def main():
+    pass
 
 if __name__ == "__main__":
-    app = Mimir()
-    app.run()
+    main()
