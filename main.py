6674dd017a547e1ca73e6ab70ade3afbc84bc2d6
Rami Saarivuori <37345715+Appelsiini1@users.noreply.github.com>
2023-01-17 15:56:43 +0200

    Add main entry point to program

--- main.py
+++ main.py
@@ -3,10 +3,29 @@ Mímir Main
 Functions to start Mímir
 """
 
-from src.tex_generator import tex_gen, write_tex_file
+import dearpygui.dearpygui as dpg
+
+from src.constants import VERSION
+from src.initialize import init_environment
+from src.tex_generator import gen_one_week
+from src.ui_handler import main_window, set_style
 
 def main():
-    pass
+    """
+    Main entry point to Mímir
+    """
+    init_environment()
+    dpg.create_context()
+    dpg.create_viewport(title=f'Mimir v{VERSION}', width=1500, height=800)
+    set_style()
+    main_window()
+
+    dpg.setup_dearpygui()
+    dpg.show_viewport()
+    dpg.start_dearpygui()
+    dpg.destroy_context()
+
+
 
 if __name__ == "__main__":
     main()
