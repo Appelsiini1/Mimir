bf5b40ff6aa212b1e6b1ac7b119ac0e9a87f4fc5
Rami Saarivuori <37345715+Appelsiini1@users.noreply.github.com>
2023-01-18 14:04:00 +0200

    Switch to setup_ui shothand function in main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 """
 Mímir Main
+
 Functions to start Mímir
 """
 
@@ -8,7 +9,7 @@ import dearpygui.dearpygui as dpg
 from src.constants import VERSION
 from src.initialize import init_environment
 from src.tex_generator import gen_one_week
-from src.ui_handler import main_window, set_style
+from src.ui_handler import main_window, setup_ui
 
 def main():
     """
@@ -17,13 +18,13 @@ def main():
     init_environment()
     dpg.create_context()
     dpg.create_viewport(title=f'Mimir v{VERSION}', width=1500, height=800)
-    set_style()
+
+    setup_ui()
     main_window()
 
     dpg.setup_dearpygui()
     dpg.show_viewport()
     dpg.start_dearpygui()
-    dpg.destroy_context()
 
 
 
