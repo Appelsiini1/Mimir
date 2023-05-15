2c40f7d046ff440d2840eae3c96ecda80cfab6d6
Rami Saarivuori <37345715+Appelsiini1@users.noreply.github.com>
2023-05-15 16:40:08 +0300

    Add UI tag lists and more UI item tags

--- src/constants.py
+++ src/constants.py
@@ -49,5 +49,48 @@ UI_ITEM_TAGS["MAIN_WINDOW"] = generate_uuid()
 UI_ITEM_TAGS["ADD_ASSIGNMENT_WINDOW"] = generate_uuid()
 UI_ITEM_TAGS["OPEN_ADD_ASSINGMENT_BUTTON"] = generate_uuid()
 UI_ITEM_TAGS["WARNING_POPUP_ADD_ASSIG_WINDOW"] = generate_uuid()
+UI_ITEM_TAGS["ASSIGNMENT_TITLE"] = generate_uuid()
+UI_ITEM_TAGS["ASSIGNMENT_LECTURE_WEEK"] = generate_uuid()
+UI_ITEM_TAGS["ASSIGNMENT_NO"] = generate_uuid()
+UI_ITEM_TAGS["ASSIGNMENT_TAGS"] = generate_uuid()
+UI_ITEM_TAGS["COURSE_ID"] = generate_uuid()
+UI_ITEM_TAGS["COURSE_TITLE"] = generate_uuid()
+UI_ITEM_TAGS["COURSE_WEEKS"] = generate_uuid()
+
+# Item tag lists
+
+COURSE_GENERAL_TAGS = [
+    UI_ITEM_TAGS["COURSE_ID"],
+    UI_ITEM_TAGS["COURSE_TITLE"],
+    UI_ITEM_TAGS["COURSE_WEEKS"]
+]
+
+GENERAL_ASSIGNMENT_TAGS = [
+    UI_ITEM_TAGS["ASSIGNMENT_TITLE"],
+    UI_ITEM_TAGS["ASSIGNMENT_LECTURE_WEEK"],
+    UI_ITEM_TAGS["ASSIGNMENT_NO"],
+    UI_ITEM_TAGS["ASSIGNMENT_TAGS"],
+    UI_ITEM_TAGS["PREVIOUS_PART_CHECKBOX"],
+    UI_ITEM_TAGS["PREVIOUS_PART_COMBOBOX"],
+    UI_ITEM_TAGS["CODE_LANGUAGE_COMBOBOX"],
+    UI_ITEM_TAGS["INST_LANGUAGE_COMBOBOX"]
+    
+]
+
+VARIATION_TAGS = [
+
+]
+
+EXAMPLE_RUN_TAGS = [
+
+]
 
 # Misc constants
+
+OPEN_IX = None
+OPEN_COURSE_PATH = None
+COURSE_INFO = {
+    "course_title": None,
+    "course_id": None,
+    "course_weeks": None
+}
